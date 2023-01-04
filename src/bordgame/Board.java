47a9014a696065b56fe7cf9d1ea72327a9c9b50b
Sandro Santos <sandro.ads.dev@gmail.com>
2023-01-03 21:13:15 -0300

    Moves Rook

--- src/bordgame/Board.java
+++ src/bordgame/Board.java
@@ -30,7 +30,7 @@ public class Board {
 	}
 
 	public void placePiece(Piece piece, Position position) {
-		if (thereIsAPeace(position)) {
+		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position" + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -59,7 +59,7 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 
-	public boolean thereIsAPeace(Position position) {
+	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
